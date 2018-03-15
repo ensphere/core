e074c15383257095c2369b3c73aee53807fa8c0d
Luke Snowden <lukesnowden@Lukes-New-Mac.local>
2018-03-15 13:12:19 +0000

    added arguments

--- Ensphere/Commands/Ensphere/SearchAndReplace/Command.php
+++ Ensphere/Commands/Ensphere/SearchAndReplace/Command.php
@@ -4,15 +4,41 @@ namespace EnsphereCore\Commands\Ensphere\SearchAndReplace;
 
 use Illuminate\Console\Command as IlluminateCommand;
 use Illuminate\Support\Facades\DB;
+use Doctrine\DBAL\Schema\SchemaException;
 
 class Command extends IlluminateCommand
 {
+
+    /**
+     * @var array
+     */
+    protected $onlyTables = [];
+
+    /**
+     * @var array
+     */
+    protected $onlyColumns = [];
+
+    /**
+     * @var array
+     */
+    protected $excludeTables = [];
+
+    /**
+     * @var array
+     */
+    protected $excludeColumns = [];
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'db:search-and-replace';
+    protected $signature = 'db:search-and-replace 
+        {--only-tables= : Comma separated list of tables to use } 
+        {--only-columns= : Comma separated list of columns to use} 
+        {--exclude-tables= : Comma separated list of tables to exclude} 
+        {--exclude-columns= : Comma separated list of columns to exclude}';
 
     /**
      * The console command description.
@@ -32,12 +58,37 @@ class Command extends IlluminateCommand
     }
 
     /**
+     * @return void
+     */
+    protected function setArguments()
+    {
+        $this->onlyTables = array_filter( array_map( 'trim', explode( ',', (string) $this->option( 'only-tables' ) ) ) );
+        $this->onlyColumns = array_filter( array_map( 'trim', explode( ',', (string) $this->option( 'only-columns' ) ) ) );
+        $this->excludeTables = array_filter( array_map( 'trim', explode( ',', (string) $this->option( 'exclude-tables' ) ) ) );
+        $this->excludeColumns = array_filter( array_map( 'trim', explode( ',', (string) $this->option( 'exclude-columns' ) ) ) );
+    }
+
+    /**
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            'only-tables' => $this->onlyTables,
+            'only-columns' => $this->onlyColumns,
+            'exclude-tables' => $this->excludeTables,
+            'exclude-columns' => $this->excludeColumns,
+        ];
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
+        $this->setArguments();
         $search = $this->ask('What string would you like to search for?');
         $replace = $this->choice('Do you want to carry out a replacement or just a search?', [ 'Search only', 'Search and replace' ] );
         $replacement = false;
@@ -59,20 +110,24 @@ class Command extends IlluminateCommand
     private function searchAndReplace( $search, $replace )
     {
         $this->info( "Searching for columns..." );
-        $tables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
+        $tables = $this->filterTableNames( DB::connection()->getDoctrineSchemaManager()->listTableNames() );
         $containing = [];
         foreach( $tables as $tableName ) {
-            $columnNames = DB::getSchemaBuilder()->getColumnListing( $tableName );
+            $columnNames = $this->filterColumnNames( DB::getSchemaBuilder()->getColumnListing( $tableName ) );
             foreach( $columnNames as $columnName ) {
-                $type = DB::connection()->getDoctrineColumn( $tableName, $columnName )->getType()->getName();
-                if( in_array( $type, [ 'string', 'text' ] ) ) {
-                    $results = DB::select( DB::raw( "select * from `{$tableName}` where `{$columnName}` like BINARY '%{$search}%'" ) );
-                    if( ! empty( $results ) ) {
-                        if( isset( $containing[ $tableName ] ) ) {
-                            $containing[ $tableName ] = [];
+                try {
+                    $type = DB::connection()->getDoctrineColumn( $tableName, $columnName )->getType()->getName();
+                    if( in_array( $type, [ 'string', 'text' ] ) ) {
+                        $results = DB::select( DB::raw( "select * from `{$tableName}` where `{$columnName}` like BINARY '%{$search}%'" ) );
+                        if( ! empty( $results ) ) {
+                            if( isset( $containing[ $tableName ] ) ) {
+                                $containing[ $tableName ] = [];
+                            }
+                            $containing[ $tableName ][] = $columnName;
                         }
-                        $containing[ $tableName ][] = $columnName;
                     }
+                } catch( SchemaException $e ) {
+                    $this->error( $e->getMessage() );
                 }
             }
         }
@@ -95,18 +150,22 @@ class Command extends IlluminateCommand
     private function searchOccurrences( $search )
     {
         $this->info("Searching for `{$search}` now across every string and text field from every table...");
-        $tables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
+        $tables = $this->filterTableNames( DB::connection()->getDoctrineSchemaManager()->listTableNames() );
         $queries = [];
         foreach( $tables as $tableName ) {
-            $columnNames = DB::getSchemaBuilder()->getColumnListing( $tableName );
+            $columnNames = $this->filterColumnNames( DB::getSchemaBuilder()->getColumnListing( $tableName ) );
             foreach( $columnNames as $columnName ) {
-                $type = DB::connection()->getDoctrineColumn( $tableName, $columnName )->getType()->getName();
-                if( in_array( $type, [ 'string', 'text' ] ) ) {
-                    $queries[] = [
-                        "query" => "select * from `{$tableName}` where `{$columnName}` like BINARY '%{$search}%'",
-                        "table" => $tableName,
-                        "column" => $columnName
-                    ];
+                try {
+                    $type = DB::connection()->getDoctrineColumn( $tableName, $columnName )->getType()->getName();
+                    if( in_array( $type, [ 'string', 'text' ] ) ) {
+                        $queries[] = [
+                            "query" => "select * from `{$tableName}` where `{$columnName}` like BINARY '%{$search}%'",
+                            "table" => $tableName,
+                            "column" => $columnName
+                        ];
+                    }
+                } catch( SchemaException $e ) {
+                    $this->error( $e->getMessage() );
                 }
             }
         }
@@ -130,4 +189,33 @@ class Command extends IlluminateCommand
             }
         }
     }
+
+    /**
+     * @param $names
+     * @return array|mixed
+     */
+    protected function filterColumnNames( $names )
+    {
+        $exclude = $this->getArguments()[ 'exclude-columns' ];
+        $only = $this->getArguments()[ 'only-columns' ];
+        if( ! empty( $only ) ) {
+            return $only;
+        }
+        return array_diff( $names, $exclude );
+    }
+
+    /**
+     * @param $names
+     * @return array|mixed
+     */
+    protected function filterTableNames( $names )
+    {
+        $exclude = $this->getArguments()[ 'exclude-tables' ];
+        $only = $this->getArguments()[ 'only-tables' ];
+        if( ! empty( $only ) ) {
+            return $only;
+        }
+        return array_diff( $names, $exclude );
+    }
+
 }
